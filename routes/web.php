982233eb3fdcbe604a762c8354329deadb168032
Chung <chung19941024@gmail.com>
2022-06-19 09:34:05 +0800

    laravel challenge README.md update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+// Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
 
 Route::group([
     'middleware' => ['auth'],
